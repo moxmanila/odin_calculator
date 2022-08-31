4a2f3b46ab981de58423962946cc5accdc1e9c2a
moxmanila <martin.ochs@live.de>
2022-08-31 02:52:16 +0200

    Map textContent of corresponding Btns to Display

--- script.js
+++ script.js
@@ -31,6 +31,26 @@ const operate = function (num1, operator, num2, ) {
 };
 
 
+
+
+let one = document.getElementById('1')
+// one.addEventListener ('click', addDisplay)
+let display = document.getElementById('result')
+
+// function addDisplay() {
+//     display.textContent = one.textContent
+// }
+
+
+let numberBtn = document.querySelectorAll('.number-button')
+// Display the pressed Buttons on the Display
+for (let i =0; i<=numberBtn.length-1; i++) {
+    numberBtn[i].addEventListener ('click', function () {
+        display.textContent = numberBtn[i].textContent
+    })
+
+}
+
 console.log (operate (3,"+",4))
 console.log (operate (3,"-",4))
 console.log (operate (3,"*",4))
